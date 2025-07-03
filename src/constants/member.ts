3a22c44d46df93b4909282565f525f234c80f1e8
Jungmin Ji <jungmin.ji@icloud.com>
2025-07-03 21:42:56 +0900

    feat: my first slack app

---
+++ src/constants/member.ts
@@ -0,0 +1,3 @@
+import { MENTION } from '../constants'
+
+export const MEMBER = [MENTION.DONKEY, MENTION.LILY, MENTION.KEVIN, MENTION.HAZEL]
---
+++ src/constants/mention.ts
@@ -0,0 +1,5 @@
+export const KEVIN = '<@U0169B4EHB5>'
+export const HAZEL = '<@UTW1PC5S9>'
+export const DONKEY = '<@U02B6PF1LHX>'
+export const LILY = '<@U02BWG5AC0M>'
+export const TEAM = '<!subteam^S057D4FH2TB>'
